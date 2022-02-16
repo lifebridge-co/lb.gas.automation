a29409c4227343f56bf4241958b2b08c9a922aca
nominalrune <nominalrune.yokoyama@gmail.com>
2022-02-16 21:54:10 +0900

    msg improvement

--- calFactory/main.ts
+++ calFactory/main.ts
@@ -55,6 +55,6 @@ const main = () => {
       Log.log("Error caught @main :%s", { err });
     }
   });
-  Log.message(`@main: Finished. \n\n------------results------------\ninput:\n${JSON.stringify(sheetInterpreter.getRuleTable())}\n\noutput:\n${calendarService.getAllCalendars().map(cal => cal.toString()+'\n"rules":\n{ '+cal.rules.map(rule => '"'+rule.scope?.value+'"'+":"+'"'+rule.role+'"').join(",")+"}\n")}`);
+  Log.message(`@main: Finished. \n\n------------results------------\ninput:\n${JSON.stringify(sheetInterpreter.getRuleTable())}\n\noutput:\n${calendarService.getAllCalendars().map(cal => cal.toString()+'\n"rules": { '+cal.rules.map(rule => '\n\t"'+rule.scope?.value+'"'+":"+'"'+rule.role+'"').join(",")+"\n}").join("\n")}\n\nCalender API call: ${calendarService.getCount()} times`);
 };
 
